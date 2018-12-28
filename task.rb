6f41fa155f2e12f97e89a8848dbbabc88db7f603
KuSeMi <SKushnir@i.ua>
2018-12-28 17:45:32 +0200

    notepad v.1, working

--- task.rb
+++ task.rb
@@ -1,3 +1,6 @@
+# Подключим встроенный в руби класс Date для работы с датами
+require 'date'
+
 class Task < Post
   def initialize
     super
@@ -6,10 +9,20 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например 13.05.2013"
+    input = STDIN.gets.chomp
+
+    @due_data = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M,%S")} \n\r \n\r"
 
+    deadline = "Крайний строк: #{@due_data}"
+
+    return [deadline, @text, time_string]
   end
 end
